fc92076008f41ca466a4f7bf513f555bb02b908a
Sinchan <112364328+ethicks-x@users.noreply.github.com>
2025-04-17 17:50:43 +0530

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";           // treat all JS code as newer version
+
+// alert(3 + 3)         // this is throw an error as we are using nodejs, not browser
+
+let name = "sinchan"
+let age = 19
+
+
+/*
+ * number => 2 to power 53
+ * bigint
+ * string => ""
+ * boolean => true/false
+ * null => standalone value
+ * undefined => value not assigned
+ *
+ * object
+ */
+
+console.log(typeof undefined);              // undefined
+console.log(typeof null);                   // object
