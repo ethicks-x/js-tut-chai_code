4a6a766fb7879742dfaba6471aa55713c6f07f08
Sinchan <112364328+ethicks-x@users.noreply.github.com>
2025-04-18 23:10:33 +0530

    Operations

---
+++ 04_operations.js
@@ -0,0 +1,17 @@
+// *********************************** OPERATIONS *****************************************
+let value = 3 
+let negValue = -value 
+
+console.log(negValue)  // -3
+
+// Normal Mathematical Operations 
+console.log(2 + 2)
+console.log(2 - 2)
+console.log(2 * 2)
+console.log(2 ** 2)
+console.log(2 % 2)
+
+// Not to be done in production mode
+// Bad Practice
+console.log(+true)  // 1
+console.log(+"")    // 0
